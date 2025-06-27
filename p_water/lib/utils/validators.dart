9af4515e23230e9460c7bc8342e7faff6747f50c
Doyen Simango <1602758@students.wits.ac.za>
2025-06-27 17:03:23 +0200

    reset password changes

--- p_water/lib/utils/validators.dart
+++ p_water/lib/utils/validators.dart
@@ -12,4 +12,9 @@ class Validators {
     }
     return null;
   }
+ static bool isValidEmail(String email) {
+  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  return emailRegex.hasMatch(email);
+}
+  
 }
